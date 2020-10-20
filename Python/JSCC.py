c057126c05bb4e9710b12a47596ae6909f87c0c3
hhalsaif <hhalsaif.program@gmail.com>
2020-10-20 19:21:27 +0300

    deleted unnecesary file

--- Python/JSCC.py
+++
@@ -1,8 +0,0 @@
-# # Importing the tools I need from the commPy library
-# from commpy.utilities import hamming_dist
-# from commpy.channels import awgn
-# import random
-# # Importing premade functions that will help with code
-# from huffman import *
-# from hamming import *
-
